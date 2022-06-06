5076d9951ceedfb99aff3621faf465233280872a
Ottera <98151813+Ottera0@users.noreply.github.com>
2022-06-06 16:19:44 +0200

    ajout livrable 2 et 6 du lot 1

---
+++ Lot 1/livrable 6/connexion.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="shortcut icon" href="images/logo.png">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" />
+    <title>Jardin de Gaïa</title>
+</head>
+
+<body>
+    <section id="home" class="home-container">
+        <header id="header" class="header">
+            <a href="index.php"> <img src="images/logo.png" alt="logo" class ="logo"> </a>
+            <ul class="nav-links">
+                <li>
+                    <a href="index.php" data-text="Accueil">Accueil</a>
+                </li>
+                <li>
+                    <a href="produits.php" data-text="Nos plantes">Nos plantes</a>
+                </li>
+                <li>
+                    <a href="details_techniques.php" data-text="Détails techniques">Détails techniques</a>
+                </li>
+                <li >
+                    <a href="panier.php" data-text="Panier">Panier</a>
+                </li>
+                <li>
+                    <a href="connexion.php" data-text="Inscription/Connexion">Inscription/Connexion</a>
+                </li>
+                <li>
+                    <form action="produits.php">
+                        <input type="search" placeholder="  Rechercher">
+                    </form>
+                </li>
+
+
+            </ul>
+
+        </header>
+        <div class="landing">
+            <h1 class="big-title">Cultivez votre bien-être.</h1>
+            <a href="#panier" class="delivery-link">
+                <span class="scroll-text" data-text="Scroll">Scroll</span>
+                <i class="scroll-icon fas fa-angle-down"></i>
+            </a>
+        </div>
+    </section>
+
+    <section id="connexion" class="connexion">
+        <h1 class="section-title">Connexion</h1>
+        <div>
+            <p>Cliquez <a href="inscription.php" class="">ici</a> pour creer votre compte.</p>
+        </div>
+        <form method="post">
+            <input type="email" name="email" id="email" placeholder="exemple@gmail.com" required>
+            <input type="password" name="password" id="password" placeholder="password" required>
+            <input type="submit" name="connexion" id="connexion">
+        </form>
+    <?php
+        include 'database.php';
+        global $db;
+
+
+
+        if(isset($_POST['connexion'])){
+            echo"formulaire sent";
+        }
+    ?>
+
+    </section>
+
+    
+    <footer id="footer" class="footer">
+        <span class="copyright">&copy; 2022 - Jardin de Gaïa </span>
+        <a href="qui_sommes_nous.php" class="team">Qui Sommes-nous ?</a>
+    </footer>
